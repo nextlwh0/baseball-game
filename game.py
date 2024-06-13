86bf926e355c333017fc051597a647049d153bf6
whlee <nextlwh@daum.net>
2024-06-13 14:56:15 +0900

    [refactoring] 66 method extraction when solved or unsolved

--- game.py
+++ game.py
@@ -8,17 +8,26 @@ class Game:
 
     def guess(self, guessNumber) -> GameResult:
         self.assert_illegal_value(guessNumber)
-        if guessNumber == self.question:
-            return GameResult(True, 3, 0)
+        if self.is_solved(guessNumber):
+            return self.get_success_game_result()
         else:
-            strikes = 0
-            balls = 0
-            for i in range(len(self.question)):
-                if self.question.find(guessNumber[i]) == i:
-                    strikes += 1
-                elif self.question.find(guessNumber[i]) > -1:
-                    balls += 1
-            return GameResult(False, strikes, balls)
+            return self.get_unsolved_game_result(guessNumber)
+
+    def get_unsolved_game_result(self, guessNumber):
+        strikes = 0
+        balls = 0
+        for i in range(len(self.question)):
+            if self.question.find(guessNumber[i]) == i:
+                strikes += 1
+            elif self.question.find(guessNumber[i]) > -1:
+                balls += 1
+        return GameResult(False, strikes, balls)
+
+    def get_success_game_result(self):
+        return GameResult(True, 3, 0)
+
+    def is_solved(self, guessNumber):
+        return guessNumber == self.question
 
     def assert_illegal_value(self, guessNumber):
         if guessNumber is None:
