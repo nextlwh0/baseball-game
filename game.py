720af28f459bcac7bd475e396e845b82f505ae9e
whlee <nextlwh@daum.net>
2024-06-13 14:07:31 +0900

    [refactoring] 43 simplify guess function

--- game.py
+++ game.py
@@ -1,5 +1,8 @@
 class Game:
     def guess(self, guessNumber):
+        self.assert_illegal_value(guessNumber)
+
+    def assert_illegal_value(self, guessNumber):
         if guessNumber is None:
             raise TypeError()
         if len(guessNumber) != 3:
@@ -7,7 +10,10 @@ class Game:
         for number in guessNumber:
             if not ord('0') <= ord(number) <= ord('9'):
                 raise TypeError()
-        if guessNumber[0] == guessNumber[1] or \
-            guessNumber[0] == guessNumber[2] or \
-            guessNumber[1] == guessNumber[2]:
+        if self.isDuplicatedNumber(guessNumber):
             raise TypeError()
+
+    def isDuplicatedNumber(self, guessNumber):
+        return guessNumber[0] == guessNumber[1] or \
+            guessNumber[0] == guessNumber[2] or \
+            guessNumber[1] == guessNumber[2]
