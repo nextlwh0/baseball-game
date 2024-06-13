dd81b87820477598b6989ff3f4d58bdd707f2355
whlee <nextlwh@daum.net>
2024-06-13 14:15:20 +0900

    [feature] 47 game result class

--- game.py
+++ game.py
@@ -1,7 +1,13 @@
-class Game:
-    def guess(self, guessNumber):
-        self.assert_illegal_value(guessNumber)
+from game_result import GameResult
 
+
+class Game:
+    def __init__(self) -> None:
+        super().__init__()
+        self.question = ""
+    def guess(self, guessNumber) -> GameResult:
+        self.assert_illegal_value(guessNumber)
+        return GameResult(True, 3, 0)
     def assert_illegal_value(self, guessNumber):
         if guessNumber is None:
             raise TypeError()
---
+++ game_result.py
@@ -0,0 +1,7 @@
+class GameResult:
+    def __init__(self, solved, strikes, balls):
+        self.balls = balls
+        self.strikes = strikes
+        self.solved = solved
+
+    pass
