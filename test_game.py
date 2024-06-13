968cfd41c61339020a992c92d0c5340a61ef788b
whlee <nextlwh@daum.net>
2024-06-13 14:01:23 +0900

    [refactoring] 37 test function 중복코드 제거

--- test_game.py
+++ test_game.py
@@ -8,11 +8,16 @@ class TestGame(TestCase):
         self.game = Game()
 
         super().setUp()
+    def assert_illegal_argument(self, guessNumber):
+        try:
+            self.game.guess(guessNumber)
+            self.fail()
+        except TypeError:
+            pass
 
-    def test_exception_when_input_is_none(self):
-        with self.assertRaises(TypeError):
-            self.game.guess(None)
 
     def test_exception_when_input_length_is_unmatched(self):
-        with self.assertRaises(TypeError):
-            self.game.guess("12")
+        self.assert_illegal_argument(None)
+        self.assert_illegal_argument("12")
+        self.assert_illegal_argument("1234")
+
